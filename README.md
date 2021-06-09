58864401e678b9752e1cb3f877816a50f29ed2c3
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-06-08 23:02:16 -0700

    Move around README, update setup.py

--- README.md
+++
@@ -1,6 +0,0 @@
-# yaml2toml
-Converts yaml files to toml files
-
-Project: https://github.com/hiljusti/yaml2toml
-
-_MIT License_
--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
-import pathlib
-from setuptools import setup
-
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
+from distutils.core import setup
 
 setup(
     name="yaml2toml",
     version="1.0.0",
     description="Convert YAML to TOML",
-    long_description=README,
-    long_description_content_type="text/markdown",
     url="https://github.com/hiljusti/yaml2toml",
     author="J.R. \"hiljusti\" Hill",
     author_email="hiljusti@pm.me",
@@ -22,6 +13,5 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     scripts=["yaml2toml"],
-    include_package_data=True,
-    install_requires=["yaml", "toml"],
 )
+
